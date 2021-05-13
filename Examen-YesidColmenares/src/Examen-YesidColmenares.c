1d9dd6e5dcbe03f427be0e8fd68255bbdd742332
Yesid Colmenares <rcolmenaresmoreno702@gmail.com>
2021-05-13 09:01:41 -0300

    Inicio

---
+++ Examen-YesidColmenares/src/Examen-YesidColmenares.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Examen-YesidColmenares.c
+ Author      : Yesid Colmenares
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+}
